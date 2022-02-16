8b4a43e9dc43f81d30f891e8b6ba89a7a9a2dbfa
nicocran1911 <93523661+nicocran1911@users.noreply.github.com>
2022-02-16 16:01:21 +0000

    Add files via upload

---
+++ index_test.js
@@ -0,0 +1,35 @@
+var assert = require("assert");
+var Calculate =  require('../index.js')
+
+describe('Calculate', () => {
+  describe('.factorial', () => {
+     it('returns correct value of 5!', () => {
+      
+
+      // setup
+      const inputNumber = 5;
+      const expectedResult = 120;
+
+      // exercise
+      const result = Calculate.factorial(inputNumber)
+
+       // verify
+       assert.equal(result, expectedResult);
+   });
+     it('returns correct value of 3!', () => {
+           
+     // setup
+     const inputNumber = 3;
+     const expectedResult = 6;
+
+     // exercise
+     const result = Calculate.factorial(inputNumber);
+
+     // verify
+      assert.equal(result, expectedResult);
+   });
+   it('returns correct value of 0!', () => {
+     assert.equal(Calculate.factorial(0), 1)
+   });
+  });
+});
